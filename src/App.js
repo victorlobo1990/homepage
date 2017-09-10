00e52c34391c961f2f46598c1573dbae1c6c64af
victorlobo1990 <victorlobo1990@gmail.com>
2017-09-09 23:31:15 -0400

    test audio player

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import song from './song.mp3';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        console.log(song);
+        return (
+            <div className="App">
+                <h1>Victor</h1>
+                <audio src="./song.mp3" >audio player</audio>
+            </div>
+        );
+    }
 }
 
 export default App;
